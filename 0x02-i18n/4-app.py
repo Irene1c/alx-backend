017eef8af1a994bc6af8487276be02a81492bb8c
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-11-02 02:44:00 +0000

    correct rendered template

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -36,7 +36,7 @@ def get_locale():
 def hello():
     """ Simple template rendering """
 
-    return render_template("3-index.html")
+    return render_template("4-index.html")
 
 
 if __name__ == '__main__':
