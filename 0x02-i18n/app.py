d5c7fd84f20c707a649fe72fb3a9b6f3c7dd1d44
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-11-02 10:57:22 +0000

    Display the current time based on the inferred time zone

---
+++ 0x02-i18n/app.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+""" A basic Flask app that implements i18n with flask_babel"""
+from datetime import datetime
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+import pytz
+import pytz.exceptions
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """ app configurations """
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+# load configuration settings
+app.config.from_object(Config)
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user():
+    """ Function that mocks Logging in """
+
+    user_id = request.args.get('login_as')
+
+    if user_id and int(user_id) in users:
+        return users[int(user_id)]
+    else:
+        return None
+
+
+@app.before_request
+def before_request():
+    """ function to execute before all other route functions """
+
+    user = get_user()
+
+    if user is not None:
+        g.user = user
+
+
+@babel.localeselector
+def get_locale():
+    """ Function get user’s preferred locale if it is supported"""
+
+    r_locale = request.args.get('locale')
+
+    # Locale from URL parameters
+    if r_locale and r_locale in app.config['LANGUAGES']:
+        return r_locale
+
+    # Locale from user settings
+    try:
+        if g.user and g.user['locale'] in app.config['LANGUAGES']:
+            return g.user['locale']
+    except AttributeError:
+        pass
+
+    # Locale from request header
+    h_locale = request.accept_languages.best_match(app.config['LANGUAGES'])
+    if h_locale:
+        return h_locale
+
+    # Default locale
+    return app.config['BABEL_DEFAULT_LOCALE']
+
+
+@babel.timezoneselector
+def get_timezone():
+    """ Function to determine appropriate time zone """
+
+    r_timezone = request.args.get('timezone')
+
+    # Find timezone parameter in URL parameters
+    try:
+        if r_timezone:
+            pytz.timezone(r_timezone)  # validate that it's a valid timezone
+            return r_timezone
+    except pytz.exceptions.UnknownTimeZoneError:
+        pass
+
+    # Find time zone from user settings
+    try:
+        if g.user:
+            pytz.timezone(g.user['timezone'])  # validate timezone
+            return g.user['timezone']
+    except (pytz.exceptions.UnknownTimeZoneError, AttributeError):
+        pass
+
+    # Default to UTC
+    return app.config['BABEL_DEFAULT_TIMEZONE']
+
+
+@app.route('/')
+def hello():
+    """ Set current time based on the inferred time zone
+        and template rendering
+    """
+
+    # Get inferred time zone
+    user_timezone = get_timezone()
+
+    try:
+        tz = pytz.timezone(user_timezone)
+    except pytz.exceptions.UnknownTimeZoneError:
+        tz = pytz.UTC  # default to UTC
+
+    current_time = datetime.now(tz)
+
+    # datetime formats for English and French
+    en_format = "%b %d, %Y, %I:%M:%S %p"
+    fr_format = "%d %b %Y à %H:%M:%S"
+
+    formatted_time = current_time.strftime(en_format)  # en by default
+
+    # If the user's preferred locale is fr
+    if get_locale() == 'fr':
+        formatted_time = current_time.strftime(fr_format)
+
+    return render_template("index.html", current_time=formatted_time)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
