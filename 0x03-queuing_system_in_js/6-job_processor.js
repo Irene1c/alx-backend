528629f7a85460ed4ffed27f1e09e5061cc0b328
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-12-05 12:08:16 +0000

    Create the Job processor

---
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -0,0 +1,13 @@
+// Create the Job processor with `kue`
+
+import { createQueue } from 'kue';
+const queue = createQueue();
+
+const sendNotification = (phoneNumber, message) => {
+  console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+};
+
+queue.process('push_notification_code', (job, done) => {
+  sendNotification(job.data.phoneNumber, job.data.message);
+  done();
+});
